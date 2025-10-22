39b5186162514d9a76f242bf213bfcac01dcab80
Srilekha <srilekhagovvala441@gmail.com>
2025-10-22 22:53:19 +0530

    #DOM comparision using Vanilla JS and React component

--- react-hooks-examples/src/App.jsx
+++ react-hooks-examples/src/App.jsx
@@ -3,6 +3,7 @@ import StudentSearchApp from './components/Debounce_Memorization';
 import TodoListReactMemo from './components/ReactMemo';
 import ToDoList from './components/ToDoList';
 import TwoCOunter from './components/TwoCounter';
+import ReactVersion from './components/VanillaJs/ReactVersion';
 function App() {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', minHeight: '100vh', width: '100vw' }}>
@@ -16,6 +17,8 @@ function App() {
       <TodoListReactMemo/>
       <h1>Debounce_Memorization</h1>
       <StudentSearchApp/>
+      <h1>The DOM</h1>
+      <ReactVersion/>
     </div>
   );
 }
---
+++ react-hooks-examples/src/components/VanillaJs/ReactVersion.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+function Counter(){
+    //step1: react manages the state
+    const [count,setCount]=useState(0)
+    //step2: JSX describes the UI(HTML-like)
+    return(
+        <>
+        <div style={{textAlign:"center"}}>
+            <h1>Counter:{count}</h1>
+            <button onClick={()=>{setCount(count+1)}}>+</button>
+            <button onClick={()=>{setCount(count-1)}}>-</button>
+        </div>
+        </>
+    )
+}
+export default Counter;
+/*⚙️ What’s happening:
+
+You don’t touch the DOM manually.
+
+React automatically re-renders the UI whenever count changes.
+
+The HTML-like syntax (<h1>, <button>) is JSX, compiled by Babel into JavaScript.
+
+✅ Pros:
+
+React handles DOM updates efficiently.
+
+Cleaner and more readable for UI building.
+
+Scales easily with components and state.
+
+❌ Cons:
+
+Slightly heavier (needs setup with Vite, React, Babel).
+
+Overkill for very small apps. */
